6c7eafbf783a8d2bd531bb39955a9d2928e607ec
Xylar Asay-Davis <xylarstorm@gmail.com>
2017-01-29 18:01:16 +0100

    Change how analyses are turned on/off ('generate')
    With this merge, there is a single list 'generate' in the
    '[output]' section that determines which analyses are run.  This
    option replaces the 'generate' options in in the sections for
    each specific analysis module.
    
    The syntax for generate is described in config.template as follows.
    
    'generate' is a list of which analyses to generate.  Valid names are:
       'ohc_timeseries', 'sst_timeseries', 'sst_modelvsobs', 'sss_modelvsobs',
       'mld_modelvsobs', 'seaice_timeseries', 'seaice_modelvsobs'
     the following shortcuts exist:
       'all' -- all analyses will be run
       'all_timeseries' -- all time-series analyses will be run
       'all_modelvsobs' -- all analyses comparing model climatologies with
                           observatoins will be run
       'all_ocean' -- all ocean analyses will be run
       'all_seaice' -- all sea-ice analyses will be run
       'no_ohc_timeseries' -- skip the 'ohc_timeseries' (and similarly with the other analyses).
       'no_ocean', 'no_timeseries', etc. -- in analogy to 'all_*', skip the given category of analysis
    
    With a command line argument `--generate`, a comma-separated list
    can be used to select which analyses are performed.  The command-line
    argument overrides the config option.

--- run_analysis.py
+++ run_analysis.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 """
-Runs MPAS-Analysis via configuration file `config.analysis` specifying analysis
-options.
+Runs MPAS-Analysis via a configuration file (e.g. `config.analysis`)
+specifying analysis options.
 
 Author: Xylar Asay-Davis, Phillip J. Wolfram
-Last Modified: 12/06/2016
+Last Modified: 01/29/2017
 """
 
 import os
-import sys
 import matplotlib as mpl
+import argparse
 
 from mpas_analysis.configuration.MpasAnalysisConfigParser \
     import MpasAnalysisConfigParser
@@ -39,6 +39,33 @@ def makedirs(inpath):  # {{{
     if not os.path.exists(inpath):
         os.makedirs(inpath)
     return inpath  # }}}
+
+
+def check_generate(config, analysisName, mpasCore, analysisCategory=None):
+    # {{{
+    """
+    determine if a particular analysis of a particular core and (optionally)
+    category should be generated.
+    """
+    generateList = config.getExpression('output', 'generate')
+    generate = False
+    for element in generateList:
+        if '_' in element:
+            (prefix, suffix) = element.split('_', 1)
+        else:
+            prefix = element
+            suffix = None
+
+        if prefix == 'all':
+            if (suffix in [mpasCore, analysisCategory]) or (suffix is None):
+                generate = True
+        elif prefix == 'no':
+            if suffix in [analysisName, mpasCore, analysisCategory]:
+                generate = False
+        elif element == analysisName:
+            generate = True
+
+    return generate  # }}}
 
 
 def analysis(config):  # {{{
@@ -71,12 +98,15 @@ def analysis(config):  # {{{
         path_existence(config, 'paths', 'ref_archive_v0_ocndir')
         path_existence(config, 'paths', 'ref_archive_v0_seaicedir')
 
-    generate_seaice_timeseries = config.getboolean('seaice_timeseries',
-                                                   'generate')
+    generate_seaice_timeseries = check_generate(
+        config, analysisName='seaice_timeseries',  mpasCore='seaice',
+        analysisCategory='timeseries')
     seaice_compare_obs = config.getboolean('seaice_timeseries',
                                            'compare_with_obs')
-    generate_seaice_modelvsobs = config.getboolean('seaice_modelvsobs',
-                                                   'generate')
+    generate_seaice_modelvsobs = check_generate(
+        config, analysisName='seaice_modelvsobs',  mpasCore='seaice',
+        analysisCategory='modelvsobs')
+
     if (generate_seaice_timeseries and seaice_compare_obs) or \
             generate_seaice_modelvsobs:
         # we will need sea-ice observations.  Make sure they're there
@@ -95,69 +125,78 @@ def analysis(config):  # {{{
     # analysis can only be imported after the right MPL renderer is selected
 
     # GENERATE OCEAN DIAGNOSTICS
-    if config.getboolean('ohc_timeseries', 'generate'):
+    if check_generate(config, analysisName='ohc_timeseries', mpasCore='ocean',
+                      analysisCategory='timeseries'):
         print ""
         print "Plotting OHC time series..."
         from mpas_analysis.ocean.ohc_timeseries import ohc_timeseries
         ohc_timeseries(config, streamMap=oceanStreamMap,
                        variableMap=oceanVariableMap)
 
-    if config.getboolean('sst_timeseries', 'generate'):
+    if check_generate(config, analysisName='sst_timeseries', mpasCore='ocean',
+                      analysisCategory='timeseries'):
         print ""
         print "Plotting SST time series..."
         from mpas_analysis.ocean.sst_timeseries import sst_timeseries
         sst_timeseries(config, streamMap=oceanStreamMap,
                        variableMap=oceanVariableMap)
 
-    if config.getboolean('nino34_timeseries', 'generate'):
+    if check_generate(config, analysisName='nino34_timeseries',
+                      mpasCore='ocean', analysisCategory='timeseries'):
         print ""
         print "Plotting Nino3.4 time series..."
         # from mpas_analysis.ocean.nino34_timeseries import nino34_timeseries
         # nino34_timeseries(config)
 
-    if config.getboolean('mht_timeseries', 'generate'):
+    if check_generate(config, analysisName='mht_timeseries', mpasCore='ocean',
+                      analysisCategory='timeseries'):
         print ""
         print "Plotting Meridional Heat Transport (MHT)..."
         # from mpas_analysis.ocean.mht_timeseries import mht_timeseries
         # mht_timeseries(config)
 
-    if config.getboolean('moc_timeseries', 'generate'):
+    if check_generate(config, analysisName='moc_timeseries', mpasCore='ocean',
+                      analysisCategory='timeseries'):
         print ""
         print "Plotting Meridional Overturning Circulation (MOC)..."
         # from mpas_analysis.ocean.moc_timeseries import moc_timeseries
         # moc_timeseries(config)
 
-    if config.getboolean('sst_modelvsobs', 'generate'):
+    if check_generate(config, analysisName='sst_modelvsobs', mpasCore='ocean',
+                      analysisCategory='modelvsobs'):
         print ""
         print "Plotting 2-d maps of SST climatologies..."
         from mpas_analysis.ocean.ocean_modelvsobs import ocn_modelvsobs
         ocn_modelvsobs(config, 'sst', streamMap=oceanStreamMap,
                        variableMap=oceanVariableMap)
 
-    if config.getboolean('mld_modelvsobs', 'generate'):
+    if check_generate(config, analysisName='mld_modelvsobs', mpasCore='ocean',
+                      analysisCategory='modelvsobs'):
         print ""
         print "Plotting 2-d maps of MLD climatologies..."
         from mpas_analysis.ocean.ocean_modelvsobs import ocn_modelvsobs
         ocn_modelvsobs(config, 'mld', streamMap=oceanStreamMap,
                        variableMap=oceanVariableMap)
 
-    if config.getboolean('sss_modelvsobs', 'generate'):
+    if check_generate(config, analysisName='sss_modelvsobs', mpasCore='ocean',
+                      analysisCategory='modelvsobs'):
         print ""
         print "Plotting 2-d maps of SSS climatologies..."
         from mpas_analysis.ocean.ocean_modelvsobs import ocn_modelvsobs
         ocn_modelvsobs(config, 'sss', streamMap=oceanStreamMap,
                        variableMap=oceanVariableMap)
 
-
     # GENERATE SEA-ICE DIAGNOSTICS
-    if config.getboolean('seaice_timeseries', 'generate'):
+    if check_generate(config, analysisName='seaice_timeseries',
+                      mpasCore='seaice', analysisCategory='timeseries'):
         print ""
         print "Plotting sea-ice area and volume time series..."
         from mpas_analysis.sea_ice.timeseries import seaice_timeseries
         seaice_timeseries(config, streamMap=seaIceStreamMap,
                           variableMap=seaIceVariableMap)
 
-    if config.getboolean('seaice_modelvsobs', 'generate'):
+    if check_generate(config, analysisName='seaice_modelvsobs',
+                      mpasCore='seaice', analysisCategory='modelvsobs'):
         print ""
         print "Plotting 2-d maps of sea-ice concentration and thickness " \
             "climatologies..."
@@ -174,14 +213,27 @@ def analysis(config):  # {{{
 
 if __name__ == "__main__":
 
-    # process command line arguments and run analysis from configuration
-    if len(sys.argv) <= 1:
-        print "usage: %s <in_config_file> [<in_config_file2>]" % sys.argv[0]
-        exit(1)
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-g", "--generate", dest="generate",
+                        help="A list of analysis modules to generate "
+                        "(nearly identical generate option in config file).",
+                        metavar="ANALYSIS1[,ANALYSIS2,ANALYSIS3,...]")
+    parser.add_argument('configFiles', metavar='CONFIG',
+                        type=str, nargs='+', help='config file')
+    args = parser.parse_args()
 
-    configFileNames = sys.argv[1:]
     config = MpasAnalysisConfigParser()
-    config.read(configFileNames)
+    config.read(args.configFiles)
+
+    if args.generate:
+        # overwrite the 'generate' in config with a string that parses to
+        # a list of string
+        generateList = args.generate.split(',')
+        generateString = ', '.join(["'{}'".format(element)
+                                    for element in generateList])
+        generateString = '[{}]'.format(generateString)
+        config.set('output', 'generate', generateString)
 
     analysis(config)
 
